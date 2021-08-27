837bd812f714c8ef63dc72e81fd049961674ccdc
kim jung jin <[jungks9351@gmail.com]>
2021-08-27 16:14:04 +0900

    feat: bookmark 제거 함수 추가

--- public/js/bookmark.js
+++ public/js/bookmark.js
@@ -2,6 +2,7 @@
 
 const $logoutBtn = document.querySelector('#logoutBtn');
 const $bookmarkList = document.querySelector('#bookmarkList');
+
 //logout
 
 const token = localStorage.getItem('token');
@@ -24,12 +25,42 @@ const getBookmarks = async () => {
 
   bookmarks.forEach((bookmark) => {
     const poster = bookmark.poster;
+    const movieId = bookmark.movieId;
     const $li = document.createElement('li');
     const $img = document.createElement('img');
+    const $div = document.createElement('div');
+    const $btn = document.createElement('button');
+    $div.classList.add('bookmark');
+    $btn.classList.add('bookmark-btn', 'fas', 'fa-heart', 'bookmarked');
+    $btn.setAttribute('id', `${movieId}`);
     $img.setAttribute('src', `${poster}`);
     $li.classList.add('bookmark-item');
+    $div.appendChild($btn);
     $li.appendChild($img);
+    $li.appendChild($div);
     $bookmarkList.appendChild($li);
-  })
+    
+  });
+  const $bookmarkBtns = document.querySelectorAll('.bookmark-btn');
+  // console.log($bookmarkBtns);
+  deleteBookmark($bookmarkBtns);
 };
 getBookmarks();
+
+const deleteBookmark = (bookmarkBtns) => {
+  bookmarkBtns.forEach((bookmarkBtn) => {
+    bookmarkBtn.onclick=  async ({target}) => {
+      if (!target.classList.contains('bookmarked')) return;
+      const bookmarkId = target.id;
+      const token = localStorage.getItem('token');
+      const res = await fetch(`http://localhost:7000/api/bookmark?id=${bookmarkId}`, 
+      {
+        method: 'DELETE',
+        headers: {'Authorization': `${token}`}
+      });
+      if (res.status === 200) {
+        bookmarkBtn.classList.toggle('bookmarked');
+      }
+    }
+  })
+}
